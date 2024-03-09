4c823182de42c462ae763b8c7c2fb6e47145fea6
ja-m3s <Redacted>
2024-03-09 15:45:01 +0000

    added aws creds

--- update_circleci_env.sh
+++ update_circleci_env.sh
@@ -7,6 +7,10 @@
 CIRCLE_API_TOKEN=$(pass circleci.com/PERSONAL_API_TOKENS/UPDATE_CIRCLECI_FROM_TERRAFORM);
 CIRCLE_PROJECT_SLUG=$(pass circleci.com/PROJECT_SLUG);
 
+AWS_ACCESS_KEY=$(pass aws/aws-cli-user/access_key)
+AWS_SECRET_KEY=$(pass aws/aws-cli-user/secret_key)
+AWS_REGION=$(pass aws/aws-cli-user/region)
+
 FTP_USER_PRIVATE_KEY=$(terraform output -raw private_key)
 FTP_USER_NAME=$(terraform output -raw ftp_user)
 FTP_SERVER_ENDPOINT=$(terraform output -raw endpoint)
@@ -45,4 +49,31 @@ curl --request POST \
   --data '{
     "name": "FTP_SERVER_ENDPOINT",
     "value": "'"$FTP_SERVER_ENDPOINT"'"
+  }'
+
+  curl --request POST \
+  --url "https://circleci.com/api/v2/project/${CIRCLE_PROJECT_SLUG}/envvar" \
+  --header "Circle-Token: $CIRCLE_API_TOKEN" \
+  --header 'content-type: application/json' \
+  --data '{
+    "name": "AWS_ACCESS_KEY",
+    "value": "'"$AWS_ACCESS_KEY"'"
+  }'
+
+  curl --request POST \
+  --url "https://circleci.com/api/v2/project/${CIRCLE_PROJECT_SLUG}/envvar" \
+  --header "Circle-Token: $CIRCLE_API_TOKEN" \
+  --header 'content-type: application/json' \
+  --data '{
+    "name": "AWS_SECRET_KEY",
+    "value": "'"$AWS_SECRET_KEY"'"
+  }'
+
+  curl --request POST \
+  --url "https://circleci.com/api/v2/project/${CIRCLE_PROJECT_SLUG}/envvar" \
+  --header "Circle-Token: $CIRCLE_API_TOKEN" \
+  --header 'content-type: application/json' \
+  --data '{
+    "name": "AWS_REGION",
+    "value": "'"$AWS_REGION"'"
   }'
