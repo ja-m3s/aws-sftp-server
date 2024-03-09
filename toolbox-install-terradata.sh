fc4f90699657d7a86b7383f0cf69e07ad6e29ec3
ja-m3s <Redacted>
2024-03-09 13:24:14 +0000

    added helper shell script which configures local environment

---
+++ toolbox-install-terradata.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#create toolbox with aws/terraform/git environment in Fedora Silverblue (convenience script)
+
+#terraform
+toolbox run sudo dnf update -y
+toolbox run sudo dnf install -y dnf-plugins-core
+toolbox run sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+toolbox run sudo dnf -y install terraform
+toolbox run sudo dnf install -y git 
+toolbox run sudo dnf install -y awscli2 
+
+//TODO
+//Programmatic access setup i.e. aws configure
+
